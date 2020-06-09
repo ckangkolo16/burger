9b7bb5577529301acafabfba496900b3022d09af
Chobu Nkolo-Kangkolo <ckangkolo16@yahoo.com>
2020-06-08 19:20:52 -0500

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+-- Insert a set of records.
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Hamburger');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Cheeseburger');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Turkey Burger');
