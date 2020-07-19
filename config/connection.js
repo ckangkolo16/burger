2644a958b370b3838e1748bb561dc6dc6b3f414b
Chobu Nkolo-Kangkolo <ckangkolo16@yahoo.com>
2020-07-19 09:09:49 -0500

    created new connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "burgers_db",
-});
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "password",
+//   database: "burgers_db",
+// });
+
+const connection = mysql.createConnection(
+  "mysql://p9xz9zu5a7ibk5h0:f3vxy1d81phbkazf@nnmeqdrilkem9ked.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/q7ul35n14vl32sr8"
+);
 
 connection.connect(function (err) {
   if (err) {
